7ce031b5c910d85418378f899f536d26561807b1
ninjafrmtjk <tazumi46.16@gmail.com>
2023-08-07 11:31:22 +0500

    Added IClientService

--- src/Crm/Program.cs
+++ src/Crm/Program.cs
@@ -1,26 +1,62 @@
 ﻿using Crm;
-ClientService clientService = new();
-CreateClient();
+
+Console.WriteLine("Choose operation: 1 - Create client, 2 - Create Order,");
+Console.Write("Found client - 3, Delete client - 4, Break operation - 0: ");
+short s = short.Parse(Console.ReadLine());
+
+IClientService clientService = new ClientService();
 OrderService orderService = new();
-CreateOrder();
+
+switch (s)
+{
+    case 1:
+        CreateClient();
+        break;
+    case 2:
+        CreateOrder();
+        break;
+    case 3:
+        string firstName = Console.ReadLine();
+        string lastName = Console.ReadLine();
+        clientService.GetClient(firstName, lastName);
+        break;
+    // case 4:
+    //     string firstName1 = Console.ReadLine();
+    //     string lastName1 = Console.ReadLine();
+    //     clientService.DeleteClient(firstName1, lastName1);
+    //     break;
+    case 0:
+        Console.WriteLine("Breaking operation...");
+        break;
+    default:
+        Console.WriteLine("Input isn't correct, please try again");
+        break;
+}
+
 
 void CreateClient()
 {
+    Console.Write("Input name: ");
     string firstName = Console.ReadLine();
+    Console.Write("Input last name: ");
     string lastName = Console.ReadLine();
+    Console.Write("Input middle name: ");
     string middleName = Console.ReadLine();
+    Console.Write("Input age: ");
     short age = short.Parse(Console.ReadLine());
+    Console.Write("Input passport number: ");
     string passportNumber = Console.ReadLine();
+    Console.Write("Input gender: ");
     string gender = Console.ReadLine();
 
-    Client newClient = clientService.CreateClient(new() {
-        FirstName = firstName,
-        LastName = lastName,
-        MiddleName = middleName,
-        Age = age,
-        PassportNumber = passportNumber,
-        Gender = gender
-    });
+    // Client newClient = clientService.CreateClient(
+    //     firstName,
+    //     lastName,
+    //     middleName,
+    //     age,
+    //     passportNumber,
+    //     gender
+    // );
 
     Console.WriteLine("Name: {0} {1} {2}", firstName, middleName, lastName);
     Console.WriteLine("Age: {0}", age);
@@ -35,7 +71,8 @@ void CreateOrder()
     int id = int.Parse(Console.ReadLine());
     short price = short.Parse(Console.ReadLine());
 
-    Order newOrder = orderService.CreateOrder(new OrderInfo() {
+    Order newOrder = orderService.CreateOrder(new OrderInfo()
+    {
         Id = id,
         Name = name,
         Color = color,
