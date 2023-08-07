1627fba24193cd3574e36c9864f7c54eb641d113
ninjafrmtjk <tazumi46.16@gmail.com>
2023-08-07 11:47:25 +0500

    Added IClientService

--- src/Crm/ClientService.cs
+++ src/Crm/ClientService.cs
@@ -1,25 +1,80 @@
 using Crm;
-
-public sealed class ClientService
+public sealed class ClientService : IClientService
 {
-    private List<Client> clients = new List<Client>();
-    public Client CreateClient(ClientInfo clientInfo)
+    private readonly List<Client> _clients;
+    public ClientService()
     {
-        clients.Add(CreateClient(clientInfo));
-
-        return new()
+        _clients = new();
+    }
+    public Client CreateClient
+    (
+        string firstName,
+        string lastName,
+        string middleName,
+        string phoneNumber,
+        string email,
+        string password,
+        short age,
+        string passportNumber,
+        string gender
+    )
+    {
+        Client newClient = new()
         {
-            FirstName = clientInfo.FirstName,
-            LastName = clientInfo.LastName,
-            MiddleName = clientInfo.MiddleName,
-            PhoneNumber = clientInfo.PhoneNumber,
-            Email = clientInfo.Email,
-            Password = clientInfo.Password,
-            Age = clientInfo.Age,
-            PassportNumber = clientInfo.PassportNumber,
-            Gender = clientInfo.Gender
+            FirstName = firstName,
+            LastName = lastName,
+            MiddleName = middleName,
+            PhoneNumber = phoneNumber,
+            Email = email,
+            Password = password,
+            Age = age,
+            PassportNumber = passportNumber,
+            Gender = gender
         };
+
+        _clients.Add(newClient);
+
+        return newClient;
     }
 
-    
+    public Client? GetClient(string name, string surname)
+    {
+        if (name is not { Length: > 0 })
+            throw new ArgumentOutOfRangeException(nameof(name));
+
+        if (surname is not { Length: > 0 })
+            throw new ArgumentOutOfRangeException(nameof(surname));
+
+        foreach (Client client in _clients)
+        {
+            if (client.FirstName.Equals(name) && client.LastName.Equals(surname))
+                return client;
+        }
+
+        return null;
+    }
+
+    public Client? DeleteClient(string name, string surname)
+    {
+        if (name is not { Length: > 0 })
+            throw new ArgumentOutOfRangeException(nameof(name));
+
+        if (surname is not { Length: > 0 })
+            throw new ArgumentOutOfRangeException(nameof(surname));
+
+        foreach (Client client in _clients)
+        {
+            _clients.RemoveAll(client => client.FirstName == name && client.LastName == surname);
+
+            Console.WriteLine(_clients);
+        }
+
+        return null;
+    }
+
+    internal Client CreateClient(string? firstName, string? lastName, string? middleName, short age, string? passportNumber, string? gender)
+    {
+        throw new NotImplementedException();
+    }
+
 }
